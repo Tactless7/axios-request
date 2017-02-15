30d27b3e20d74e15c2c7d9437c3ab1239617cec8
Juliane Blier <blier.juliane@gmail.com>
2017-02-15 10:48:25 +0100

    Ajout du crf field

--- resources/views/sweets/add.blade.php
+++ resources/views/sweets/add.blade.php
@@ -1,10 +1,11 @@
-@extends(layouts.user)
+@extends('layouts.user')
 
 @section('content')
   <h1>Add a Sweet</h1>
   <form class="ui form" action="/add" method="post">
-    <label for="name"></label><input type="text" name="name">
-    <label for="stock"></label><input type="number" name="stock">
-    <button type="submit" name="button">Save</button>
+    <label for="name">Name</label><input type="text" name="name">
+    <label for="stock">Stock</label><input type="number" name="stock">
+      {{ csrf_field() }}
+    <button class="ui green button" type="submit" name="button">Save</button>
   </form>
 @stop
