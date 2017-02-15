53d29dae5cf4de16af09404e940130b5a6003d38
Juliane Blier <blier.juliane@gmail.com>
2017-02-15 12:01:24 +0100

    Ajout des routes api pour la modif de stock

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/sell', 'SweetController@asyncSell');
+Route::post('/restock', 'SweetController@asyncRestock');
