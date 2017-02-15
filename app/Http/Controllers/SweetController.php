69c407b23c6f770a845d5e8f6d62d39ce5d5dfc7
Juliane Blier <blier.juliane@gmail.com>
2017-02-15 11:25:00 +0100

    Ajout de la méthode list

--- app/Http/Controllers/SweetController.php
+++ app/Http/Controllers/SweetController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Sweet;
 
 class SweetController extends Controller
 {
-    //
+    public function getList(){
+      $sweets = \App\Sweet::all();
+      return view('sweets.list', ['sweets' => $sweets]);
+    }
 }
